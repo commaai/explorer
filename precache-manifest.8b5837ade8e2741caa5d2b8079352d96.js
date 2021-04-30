65ca53dba024f5821fb676863e00f6ef4ac3a193
George Hotz <george@comma.ai>
2021-04-30 16:48:38 -0700

    Revert "Updates"
    This reverts commit d4e6d7bc8e28889e00b26864b016655e675d339e.

---
+++ precache-manifest.8b5837ade8e2741caa5d2b8079352d96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917aeda1d15e9b3d230e4946babd3fa6",
+    "url": "/bbfb579bab7a2c2b26c5.worker.js"
+  },
+  {
+    "revision": "48b6de88be9813c039a8faca49607bdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c9ccb8ea9f846438dde",
+    "url": "/static/css/2.1dcfeb56.chunk.css"
+  },
+  {
+    "revision": "3ec3c45d1b057bb60f6a",
+    "url": "/static/css/main.a16d780c.chunk.css"
+  },
+  {
+    "revision": "7c9ccb8ea9f846438dde",
+    "url": "/static/js/2.7c9f4819.chunk.js"
+  },
+  {
+    "revision": "3ec3c45d1b057bb60f6a",
+    "url": "/static/js/main.c014e3b5.chunk.js"
+  },
+  {
+    "revision": "6e9ba062b08c9d8eb9c5",
+    "url": "/static/js/runtime-main.9e2219b8.js"
+  },
+  {
+    "revision": "368fbfd61326403ee20f91629f2af514",
+    "url": "/static/media/icon-chffr-wheel.368fbfd6.svg"
+  }
+]);
