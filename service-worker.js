cd85cdce1e7d2b949153f2c45ec0c922eb0ddf22
George Hotz <george@comma.ai>
2021-04-30 16:48:36 -0700

    Revert "Updates"
    This reverts commit 86fb59b7bb545c392e9c8c3bda99322a9e35e9b5.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://commaai.github.io/explorer/precache-manifest.4479103588bfbf46ba41ee0666294561.js"
+  "/precache-manifest.e4f5ecfda3e267ead75a47b727c856c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://commaai.github.io/explorer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
