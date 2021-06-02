691fcb1fcb3cf1dd4a91b9a094db05d3b4544dcb
Joost Wooning <jwooning@gmail.com>
2021-06-02 16:20:57 +0200

    Updates

---
+++ precache-manifest.f4fd8fca0b6b57d3119f5cc8d287a44c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d2804f20e1134fa95484b4d49384ae",
+    "url": "/61f00f5c71834773a829.worker.js"
+  },
+  {
+    "revision": "239bb9d4e2845b7fee9e776944c133ed",
+    "url": "/61f00f5c71834773a829.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f238e1cf659792b258d760635948c4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5da55f1bbab52c60c411",
+    "url": "/static/css/2.83704e1f.chunk.css"
+  },
+  {
+    "revision": "6c5912b8c3fa8df798b5",
+    "url": "/static/css/main.b9a4e1e2.chunk.css"
+  },
+  {
+    "revision": "5da55f1bbab52c60c411",
+    "url": "/static/js/2.d74c510d.chunk.js"
+  },
+  {
+    "revision": "4077115ad591e59867710ab68586f132",
+    "url": "/static/js/2.d74c510d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c5912b8c3fa8df798b5",
+    "url": "/static/js/main.afa90991.chunk.js"
+  },
+  {
+    "revision": "a53c9bc4da5509bade09",
+    "url": "/static/js/runtime-main.ab3ce312.js"
+  },
+  {
+    "revision": "368fbfd61326403ee20f91629f2af514",
+    "url": "/static/media/icon-chffr-wheel.368fbfd6.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4e51019be92d1fce1ddba66bc8d1d806.js"
+  "/precache-manifest.f4fd8fca0b6b57d3119f5cc8d287a44c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
