4c2d2dc2da6ab775670399c7e1f051a0384fd04e
Joost Wooning <jwooning@gmail.com>
2021-07-05 18:40:11 +0200

    Updates

---
+++ precache-manifest.4129a221584df2674db8cc2c18b8fb4f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98048f022a9891e8f28ac2c90bd164b4",
+    "url": "/c88ed9691f5cc495bee3.worker.js"
+  },
+  {
+    "revision": "2db57f158bc36e2928e15e23cbe6e130",
+    "url": "/c88ed9691f5cc495bee3.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b63095657720dab314055b897e4fb95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ce9dca548ca9a874099",
+    "url": "/static/css/2.e06d4690.chunk.css"
+  },
+  {
+    "revision": "81a934c0a4e310e97db7",
+    "url": "/static/css/main.b9a4e1e2.chunk.css"
+  },
+  {
+    "revision": "3ce9dca548ca9a874099",
+    "url": "/static/js/2.2854f19b.chunk.js"
+  },
+  {
+    "revision": "cccd49395751e4c490307a6e296a06d6",
+    "url": "/static/js/2.2854f19b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a934c0a4e310e97db7",
+    "url": "/static/js/main.d1364ace.chunk.js"
+  },
+  {
+    "revision": "a53c9bc4da5509bade09",
+    "url": "/static/js/runtime-main.ab3ce312.js"
+  },
+  {
+    "revision": "368fbfd61326403ee20f91629f2af514",
+    "url": "/static/media/icon-chffr-wheel.368fbfd6.svg"
+  },
+  {
+    "revision": "e2cea424a9c0ea304511081d296cd6b5",
+    "url": "/static/media/pin-car.e2cea424.svg"
+  },
+  {
+    "revision": "76fbb1ef1062042e9b7524e0dec4bbfe",
+    "url": "/static/media/pin-home.76fbb1ef.svg"
+  },
+  {
+    "revision": "f23cf7515b2ef954dde26580ad370bae",
+    "url": "/static/media/pin-marker.f23cf751.svg"
+  },
+  {
+    "revision": "8e0cdc8d2852d0af85aaab9e8fcdac4c",
+    "url": "/static/media/pin-pinned.8e0cdc8d.svg"
+  },
+  {
+    "revision": "149766dad5ba493adf39efe0f0547d34",
+    "url": "/static/media/pin-work.149766da.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3ebd6f4d58f4a251787f4cfdecd37112.js"
+  "/precache-manifest.4129a221584df2674db8cc2c18b8fb4f.js"
 );
 
 self.addEventListener('message', (event) => {
